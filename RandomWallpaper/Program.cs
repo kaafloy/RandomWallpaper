7db3b53448c761c6a82467e960ed0be1e2425d71
Kenneth Aafløy <kenneth@aafloy.no>
2018-12-09 03:43:34 +0100

    Initial version

---
+++ RandomWallpaper/Program.cs
@@ -0,0 +1,38 @@
+﻿using Microsoft.Win32;
+using System;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices;
+
+namespace RandomWallpaper
+{
+	class Program
+	{
+		const int SPI_SETDESKWALLPAPER = 20;
+		const int SPIF_UPDATEINIFILE = 0x01;
+		const int SPIF_SENDWININICHANGE = 0x02;
+
+		[DllImport("user32.dll", CharSet = CharSet.Auto)]
+		static extern int SystemParametersInfo(int uAction, int uParam, string lpvParam, int fuWinIni);
+
+		public enum Style : int
+		{
+			Tiled,
+			Centered,
+			Stretched
+		}
+
+		static void Main(string[] args)
+		{
+			var picturesFolder = Environment.GetFolderPath(Environment.SpecialFolder.MyPictures);
+			var backgroundFolder = Path.Combine(picturesFolder, @"Backgrounds");
+			var backgrounds = Directory.EnumerateFiles(backgroundFolder).ToList();
+			var rnd = new Random();
+			var nextBackground = backgrounds[rnd.Next(0, backgrounds.Count)];
+
+			SystemParametersInfo(SPI_SETDESKWALLPAPER, 0,
+				nextBackground,
+				SPIF_UPDATEINIFILE | SPIF_SENDWININICHANGE);
+		}
+	}
+}
